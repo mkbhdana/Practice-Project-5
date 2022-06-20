d8edbc866e6d170a77e7f00fcd9da333d2099ff5
mkbhdana <mkbhdana@gmail.com>
2022-06-20 16:14:17 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyparser = require("body-parser");
+const mongoose = require('mongoose');
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/contactDance');
+}
+const port = 8000;
+
+// Define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    date: Date,
+    mobile: String,
+    email: String,
+    address: String,
+    desc: String
+});
+
+const Contact = mongoose.model('contact', contactSchema);
+
+
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+// ENDPOINTS
+app.get('/', (req, res) => {
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+app.get('/contact', (req, res) => {
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+
+app.post('/contact', (req, res) => {
+    let myData = new Contact(req.body);
+    myData.save().then(() => {
+        res.send("This item has been saved successfully")
+    }).catch(() => {
+        res.status(400).send("Item was not saved successfully")
+    })
+    // res.status(200).render('contact.pug');
+})
+
+// START THE SERVER
+app.listen(port, () => {
+    console.log(`The application started successfully on port ${port}`);
+});
